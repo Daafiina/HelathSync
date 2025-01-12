d285f52dc23971adc1433b789471254f4e476d77
Dafina Aliji <da52811@ubt-uni.net>
2025-01-12 21:46:32 +0100

    Added routes for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import AppointmentsPage from './pages/AppointmentsPage'
+import About from './pages/About'
+import Login from './pages/Login'
+import ProfilePage from './pages/profilePage'
+import Appointment from './pages/Appointment'
+
 
 const App = () => {
   return (
-    <div>
-     
+    <div className='mx-4 sm:mx-[15%]'>
+      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/appointment/:doctorId' element={<Appointment />}/>
+        <Route path='/appointments-page' element={<AppointmentsPage />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/profile-page' element={<ProfilePage />}/>
+
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/Appointment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Appointment = () => {
+  return (
+    <div>
+      Appointment
+      Appointment
+    </div>
+  )
+}
+
+export default Appointment
